d5a12ef54d9815126c5d839ddc0238fe3da288ed
NICOLE CANCEL <nicole.cancel_2002@yahoo.ro>
2023-10-22 21:41:45 +0300

    Add project files.

---
+++ Data/Cancel_Nicole_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cancel_Nicole_Lab2.Models;
+
+namespace Cancel_Nicole_Lab2.Data
+{
+    public class Cancel_Nicole_Lab2Context : DbContext
+    {
+        public Cancel_Nicole_Lab2Context (DbContextOptions<Cancel_Nicole_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cancel_Nicole_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cancel_Nicole_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Models/Authors.cs
@@ -0,0 +1,10 @@
+﻿namespace Cancel_Nicole_Lab2.Models
+{
+    public class Authors
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
